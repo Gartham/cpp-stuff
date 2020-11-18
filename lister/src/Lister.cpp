56ef4923ab061dacf6baf75dc8c3a8226b5612f0
Gartham <gartham@dusttoash.org>
2020-11-17 20:59:02 -0600

    Add lister project files
    <o/

---
+++ lister/src/Lister.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Lister.cpp
+// Author      : Gartham
+// Version     :
+// Copyright   : Copyright.
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Helo" << endl; // prints Helo
+	return 0;
+}
